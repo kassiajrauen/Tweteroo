d6374498525324d3be6c09fa679d70eebc7f73c2
KassiaJRauen <kassia.grj@gmail.com>
2022-01-24 15:43:40 -0300

    rota sign-up

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
-import express from 'express';
+import express, { json } from 'express';
+import cors from 'cors';
 
 const server = express();
+server.use(cors());
+server.use(json())
+
+const user = [];
 
 server.post('/sign-up', (req, res) => {
-    const signUp = {
-        username: "bobesponja",
-            avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info"
-    }
+    const signUp = req.body;
+    user.push(signUp);
 
-    res.send(signUp);
+    res.send('OK');
 })
 
 server.post('/tweets', (req, res) => {
     const tweets = {
         username: "bobesponja",
-      tweet: "eu amo o hub"
+        tweet: "eu amo o hub"
     }
 
     res.send(tweets);
@@ -25,7 +28,7 @@ server.get('/tweets', (req, res) => {
         {
             username: "bobesponja",
             avatar: "https://super.abril.com.br/wp-content/uploads/2020/09/04-09_gato_SITE.jpg?quality=70&strip=info",
-          tweet: "eu amo o hub"
+            tweet: "eu amo o hub"
         }
     ]
 
